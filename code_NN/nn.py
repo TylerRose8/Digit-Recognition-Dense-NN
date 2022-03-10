f828ab6417df033d99243c966d1b65e4a7c6ace9
Tyler Rose <TylerRose25565@gmail.com>
2022-03-10 12:10:28 -0800

    FF with error output

--- code_NN/nn.py
+++ code_NN/nn.py
@@ -3,29 +3,26 @@
 ### Delete every `pass` statement below and add in your own code. 
 
 
-
 # Implementation of the forwardfeed neural network using stachastic gradient descent via backpropagation
 # Support parallel/batch mode: process every (mini)batch as a whole in one forward-feed/backtracking round trip. 
 
 
+import math
+import time
 
 import numpy as np
-import math
-import math_util as mu
+
 import nn_layer
-from code_NN import math_util
-import time
 
 
 class NeuralNetwork:
 
     def __init__(self):
-        self.layers = []     # the list of L+1 layers, including the input layer.
-        self.L = -1          # Number of layers, excluding the input layer.
-                             # Initting it as -1 is to exclude the input layer in L.
+        self.layers = []  # the list of L+1 layers, including the input layer.
+        self.L = -1  # Number of layers, excluding the input layer.
+        # Initting it as -1 is to exclude the input layer in L.
 
-
-    def add_layer(self, d = 1, act = 'tanh'):
+    def add_layer(self, d=1, act='tanh'):
         ''' The newly added layer is always added AFTER all existing layers.
             The firstly added layer is the input layer.
             The most recently added layer is the output layer. 
@@ -42,17 +39,16 @@ class NeuralNetwork:
         self.L += 1
         self.layers.append(nn_layer.NeuralLayer(d, act))
 
-
     def _init_weights(self):
         ''' Initialize every layer's edge weights with random numbers from [-1/sqrt(d),1/sqrt(d)], 
             where d is the number of nonbias node of the layer
         '''
-        for i in range(1, self.L+1):
+        for i in range(1, self.L + 1):
             # self.layers[i].W = np.random.uniform(-1/math.sqrt(self.layers[i].d), 1/math.sqrt(self.layers[i].d), (self.layers[i-1].d+1, self.layers[i].d))
             weight_rng = np.random.default_rng(2142)
-            self.layers[i].W = weight_rng.uniform(-1/math.sqrt(self.layers[i].d), 1/math.sqrt(self.layers[i].d), (self.layers[i-1].d+1, self.layers[i].d))
+            self.layers[i].W = weight_rng.uniform(-1 / math.sqrt(self.layers[i].d), 1 / math.sqrt(self.layers[i].d), (self.layers[i - 1].d + 1, self.layers[i].d))
 
-    def _feed_forward(self, X):
+    def _feed_forward(self, X, Y=None):
         cupy_available = False
         try:
             import cupy as np
@@ -65,23 +61,25 @@ class NeuralNetwork:
             X = np.array(X)
         time_start = time.time()
         self.layers[0].X = X
-        for i in range (1, self.L+1):
-            self.layers[i-1].X = np.array(numpy.insert(self.layers[i-1].X.get(), 0, 1, axis=1))
+        for i in range(1, self.L + 1):
+            self.layers[i - 1].X = np.array(numpy.insert(self.layers[i - 1].X.get(), 0, 1, axis=1))
             current = self.layers[i]
-            previous = self.layers[i-1]
+            previous = self.layers[i - 1]
             current.W = np.array(current.W)
             current.S = previous.X @ current.W
             current.X = current.act(current.S)
         if cupy_available:
             self.layers[0].X = self.layers[0].X.get()
-            for i in range(1,self.L+1):
+            for i in range(1, self.L + 1):
                 self.layers[i].X = self.layers[i].X.get()
                 self.layers[i].S = self.layers[i].S.get()
                 self.layers[i].W = self.layers[i].W.get()
             import numpy as np
         print("Time for feed-forward: ", time.time() - time_start)
+        if Y is not None:
+            return np.sum(np.square(self.predict(X) - np.argmax(Y, axis=1))) / Y.shape[0]
 
-    def _back_propagation(self,y,N):
+    def _back_propagation(self, y, N):
         try:
             import cupy as np
             cupy_available = True
@@ -93,24 +91,24 @@ class NeuralNetwork:
             self.layers[self.L].X = np.array(self.layers[self.L].X)
             self.layers[self.L].W = np.array(self.layers[self.L].W)
             self.layers[self.L].S = np.array(self.layers[self.L].S)
-        self.layers[self.L].Delta = 2*(self.layers[self.L].X-y) * self.layers[self.L].act_de(self.layers[self.L].S)
-        self.layers[self.L].G = np.einsum('ij,ik->jk', self.layers[self.L-1].X, self.layers[self.L].Delta) * 1 / N
-        for i in range(self.L-1, 0, -1):
+        self.layers[self.L].Delta = 2 * (self.layers[self.L].X - y) * self.layers[self.L].act_de(self.layers[self.L].S)
+        self.layers[self.L].G = np.einsum('ij,ik->jk', self.layers[self.L - 1].X, self.layers[self.L].Delta) * 1 / N
+        for i in range(self.L - 1, 0, -1):
             if cupy_available:
                 self.layers[i].X = np.array(self.layers[i].X)
                 self.layers[i].W = np.array(self.layers[i].W)
                 self.layers[i].S = np.array(self.layers[i].S)
-            #self.layers[i-1].X = np.array(numpy.insert(self.layers[i-1].X, 0, 1, axis=1))
+            # self.layers[i-1].X = np.array(numpy.insert(self.layers[i-1].X, 0, 1, axis=1))
             layer = self.layers
-            layer[i].Delta = layer[i].act_de(layer[i].S) * (layer[i+1].Delta @ layer[i+1].W[1:].T)
-            layer[i].G = np.einsum('ij,ik->jk',layer[i-1].X, layer[i].Delta)*1/N
+            layer[i].Delta = layer[i].act_de(layer[i].S) * (layer[i + 1].Delta @ layer[i + 1].W[1:].T)
+            layer[i].G = np.einsum('ij,ik->jk', layer[i - 1].X, layer[i].Delta) * 1 / N
         if cupy_available:
             self.layers[self.L].X = self.layers[self.L].X.get()
             self.layers[self.L].W = self.layers[self.L].W.get()
             self.layers[self.L].S = self.layers[self.L].S.get()
             self.layers[self.L].Delta = self.layers[self.L].Delta.get()
             self.layers[self.L].G = self.layers[self.L].G.get()
-            for i in range(self.L-1, 0, -1):
+            for i in range(self.L - 1, 0, -1):
                 self.layers[i].X = self.layers[i].X.get()
                 self.layers[i].S = self.layers[i].S.get()
                 self.layers[i].W = self.layers[i].W.get()
@@ -121,7 +119,7 @@ class NeuralNetwork:
 
     def _update_weights(self, eta):
         time_start = time.time()
-        for i in range(1, self.L+1):
+        for i in range(1, self.L + 1):
             self.layers[i].W = self.layers[i].W
             self.layers[i].G = self.layers[i].G
             left = self.layers[i].W
@@ -160,11 +158,11 @@ class NeuralNetwork:
                 batch_y = np.split(batch_y, n / mini_batch_size)[itr % round(n / mini_batch_size - 0.25)]
 
                 for i in range(batch_X.shape[0]):
-                    self._feed_forward(batch_X[i])
-                    error = np.sum(np.square(self.layers[self.L].X - Y)) * 1 / (2 * len(X))
+                    error = self._feed_forward(batch_X[i], batch_y[i])
+                    # error = np.sum(np.square(self.layers[self.L].X - Y)) * 1 / (2 * len(X))
                     errors.append(error)
-                    error2 = self.error(X, Y)
-                    errors2.append(error2)
+                    # error2 = self.error(X, Y)
+                    # errors2.append(error2)
                     print('error: ', error)
                     self._back_propagation(batch_y[i], batch_X.shape[0])
                     self._update_weights(eta)
@@ -173,16 +171,16 @@ class NeuralNetwork:
             time_start = time.time()
             for itr in range(iterations):
                 print('iteration: ', itr)
-                self._feed_forward(X)
-                error = np.sum(np.square(self.layers[self.L].X - Y)) * 1 / (2 * len(X))
+                error = self._feed_forward(X, Y)
+                # error = np.sum(np.square(self.layers[self.L].X - Y)) * 1 / (2 * len(X))
                 errors.append(error)
-                error2 = self.error(X, Y)
-                errors2.append(error2)
+                # error2 = self.error(X, Y)
+                # errors2.append(error2)
                 print('error: ', error)
                 self._back_propagation(Y, X.shape[0])
                 self._update_weights(eta)
             print("Time for fit-batch: ", time.time() - time_start)
-        return errors, errors2
+        return errors
         # I will leave you to decide how you want to organize the rest of the code, but below is what I used and recommend. Decompose them into private components/functions.
 
         ## prep the data: add bias column; randomly shuffle data training set.
@@ -202,8 +200,6 @@ class NeuralNetwork:
         self._feed_forward(X)
         return np.argmax(self.layers[self.L].X, axis=1)
 
-
-
     def error(self, X, Y):
         ''' X: n x d matrix, the sample batch, excluding the bias feature 1 column. 
                n is the number of samples. 
@@ -217,5 +213,6 @@ class NeuralNetwork:
         '''
 
         Y = np.array(Y)
-        error = np.count_nonzero(self.predict(X) - np.argmax(Y,axis=1))
-        return error/Y.shape[0]
+        # error = np.count_nonzero(self.predict(X) - np.argmax(Y,axis=1))
+        error = np.sum(np.square(self.predict(X) - np.argmax(Y, axis=1)))
+        return error / Y.shape[0]
